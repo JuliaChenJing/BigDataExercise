0e66b79535a5ce83d90da800ab683ca65123bc35
Julia Chen Jing <juliachenjing@gmail.com>
2017-04-05 20:52:29 -0500

    Add files via upload

--- WID3/src/Application.java
+++ WID3/src/Application.java
@@ -5,7 +5,6 @@ import java.io.FileNotFoundException;
 import org.apache.commons.io.IOUtils;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 
@@ -16,27 +15,53 @@ public class Application {
 		
 		
 		// read the txt file to string
-		String str="";
+		List <String> inputSplits=new ArrayList<String>();
+		int numOfReducers=4;
+		
+		System.out.println("Number of Input-Splits:"+inputSplits.size() );
+		System.out.println("Number of Reducers:"+numOfReducers );
+		
 		try {
-			 str=readFile();
+			 inputSplits.add(readFile("0.txt"));
+			 inputSplits.add(readFile("1.txt"));
+			 inputSplits.add(readFile("2.txt"));
 			//System.out.println(str);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			System.out.println("---"+e.getMessage());
 		}
 		
+		
+		System.out.println("---------------------------Mapper 0 Input-------------");
+		System.out.println(inputSplits.get(0));
+		
+		System.out.println("---------------------------Mapper 1 Input-------------");
+		System.out.println(inputSplits.get(1));
+		
+		System.out.println("---------------------------Mapper 2 Input-------------");
+		System.out.println(inputSplits.get(2));
+		
 		Mapper mapper=new Mapper();
 		
-		 List <Pair>collectionOfPair=mapper.mapperOutput(str);
+		List <Pair> mapperOutput_0=mapper.mapperOutput(inputSplits.get(0));
+		List <Pair> mapperOutput_1=mapper.mapperOutput(inputSplits.get(1));
+		List <Pair> mapperOutput_2=mapper.mapperOutput(inputSplits.get(2));
 
-		System.out.println("---------------------------Mapper Output-------------");
-		System.out.println(collectionOfPair.toString());
+		System.out.println("---------------------------Mapper 0 Output-------------");
+		System.out.println(mapperOutput_0.toString());
+		
+		System.out.println("---------------------------Mapper 1 Output-------------");
+		System.out.println(mapperOutput_1.toString());
+		
+		System.out.println("---------------------------Mapper 2 Output-------------");
+		System.out.println(mapperOutput_2.toString());
+
 		
 		
 		// -Reducer Input--
 		Reducer reducer=new Reducer();
 		
-		 List <GroupByPair> collectionOfGroupByPair=reducer.reducerOutput((ArrayList)collectionOfPair);
+		 List <GroupByPair> collectionOfGroupByPair=reducer.reducerOutput((ArrayList)mapperOutput_0);
 		
 		       
 		System.out.println("---------------------------Reducer Input------------");
@@ -53,12 +78,12 @@ public class Application {
 	}
 	
 	// read the txt file and store it to a string
-   static String readFile() 
+   static String readFile(String filename) 
 	{
 		String targetFileString="";
 		FileInputStream targetFile;
 		try {
-			targetFile = new FileInputStream(new File("testDataForW1D1.txt"));
+			targetFile = new FileInputStream(new File( filename));
 			targetFileString =IOUtils.toString(targetFile, "UTF-8");
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
