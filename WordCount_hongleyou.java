93aefb0757f0d769be2dc44db99089d2d01615ae
Julia Chen Jing <juliachenjing@gmail.com>
2017-04-17 14:52:34 -0500

    Update WordCount_hongleyou.java

--- WordCount_hongleyou.java
+++ WordCount_hongleyou.java
@@ -27,7 +27,7 @@ public class WordCount {
             StringTokenizer itr = new StringTokenizer(value.toString());
             while (itr.hasMoreTokens()) {
                 word.set(itr.nextToken());
-                context.write(word, one);
+                context.write(word, one);//emit
             }
         }
     }
@@ -36,15 +36,13 @@ public class WordCount {
     public static class IntSumReducer  extends Reducer<Text, IntWritable, Text, IntWritable> {
         private IntWritable result = new IntWritable();
 
-        public void reduce(Text key, Iterable<IntWritable> values,
-                           Context context
-        ) throws IOException, InterruptedException {
+        public void reduce(Text key, Iterable<IntWritable> values,Context context) throws IOException, InterruptedException {
             int sum = 0;
             for (IntWritable val : values) {
                 sum += val.get();
             }
             result.set(sum);
-            context.write(key, result);
+            context.write(key, result);//emit
         }
     }
 
