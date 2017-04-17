6f3b1c81781cec4ec5f6b9a9999794a70a81f38e
Julia Chen Jing <juliachenjing@gmail.com>
2017-04-17 14:42:06 -0500

    Update WordCount.java

--- WordCount.java
+++ WordCount.java
@@ -1,7 +1,8 @@
 package wordCount;
+
 import java.io.IOException;
 import java.util.*;
-        
+
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.conf.*;
 import org.apache.hadoop.io.*;
@@ -10,60 +11,69 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
-        
+
 public class WordCount {
-        
-    
-        /*A non-static nested class has full access to the members of the class within which it is nested. 
-        A static nested class does not have a reference to a nesting instance, so a static nested class
-        cannot invoke non-static methods or access non-static fields of an instance of the class within which
-        it is nested.*/
-        
-           //class Map is a static nested class
-  public static class Map extends Mapper<LongWritable, Text, Text, IntWritable> {
-    private final static IntWritable one = new IntWritable(1);
-    private Text word = new Text();
-        //method map
-    public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-        String line = value.toString();
-        StringTokenizer tokenizer = new StringTokenizer(line);
-        while (tokenizer.hasMoreTokens()) {
-            word.set(tokenizer.nextToken());
-            context.write(word, one);
-        }
-    }
-  } 
-        //class Reduce is a static nested class
- public static class Reduce extends Reducer<Text, IntWritable, Text, IntWritable> {
-        //method reduce
-    public void reduce(Text key, Iterable<IntWritable> values, Context context) 
-      throws IOException, InterruptedException {
-        int sum = 0;
-        for (IntWritable val : values) {
-            sum += val.get();
-        }
-        context.write(key, new IntWritable(sum));
-    }
- }
-        //main method
- public static void main(String[] args) throws Exception {
-         
-    Configuration conf = new Configuration(); 
-    Job job = new Job(conf, "wordcount");
-    
-    job.setOutputKeyClass(Text.class);//set output key class
-    job.setOutputValueClass(IntWritable.class);//set output value class
-        
-    job.setMapperClass(Map.class);//set mapper class
-    job.setReducerClass(Reduce.class);//set reducer class
-        
-    job.setInputFormatClass(TextInputFormat.class); //st input format
-    job.setOutputFormatClass(TextOutputFormat.class);//set output format
-        
-    FileInputFormat.addInputPath(job, new Path(args[0]));//add input path
-    FileOutputFormat.setOutputPath(job, new Path(args[1]));//add output path
-        
-    job.waitForCompletion(true);
- }
-        
+
+	/*
+	 * A non-static nested class has full access to the members of the class
+	 * within which it is nested. A static nested class does not have a
+	 * reference to a nesting instance, so a static nested class cannot invoke
+	 * non-static methods or access non-static fields of an instance of the
+	 * class within which it is nested.
+	 */
+
+	// class Map is a static nested class
+	public static class Map extends
+			Mapper<LongWritable, Text, Text, IntWritable> {
+		private final static IntWritable one = new IntWritable(1);
+		private Text word = new Text();
+
+		// method map
+		public void map(LongWritable key, Text value, Context context)
+				throws IOException, InterruptedException {
+			String line = value.toString();
+			StringTokenizer tokenizer = new StringTokenizer(line);
+			while (tokenizer.hasMoreTokens()) {
+				word.set(tokenizer.nextToken());
+				context.write(word, one);
+			}
+		}
+	}
+
+	// class Reduce is a static nested class
+	public static class Reduce extends
+			Reducer<Text, IntWritable, Text, IntWritable> {
+		// method reduce
+		public void reduce(Text key, Iterable<IntWritable> values,
+				Context context) throws IOException, InterruptedException {
+			int sum = 0;
+			for (IntWritable val : values) {
+				sum += val.get();
+			}
+			context.write(key, new IntWritable(sum));
+		}
+	}
+
+	// main method
+	public static void main(String[] args) throws Exception {
+
+		Configuration conf = new Configuration();
+		Job job = new Job(conf, "wordcount");
+
+		job.setOutputKeyClass(Text.class);// set output key class
+		job.setOutputValueClass(IntWritable.class);// set output value class
+
+		job.setMapperClass(Map.class);// set mapper class
+		job.setReducerClass(Reduce.class);// set reducer class
+
+		job.setInputFormatClass(TextInputFormat.class); // st input format
+		job.setOutputFormatClass(TextOutputFormat.class);// set output format
+
+		FileInputFormat.addInputPath(job, new Path(args[0]));// add input path
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));// add output
+																// path
+
+		job.waitForCompletion(true);
+	}
+
 }
