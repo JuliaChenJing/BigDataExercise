387603e5bbf43d2f5e1dd84446a71c41bbd69d5c
Julia Chen Jing <juliachenjing@gmail.com>
2017-04-19 15:41:35 -0500

    Update PairmapperStripereducer.java

--- PairmapperStripereducer.java
+++ PairmapperStripereducer.java
@@ -17,135 +17,133 @@ import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
-
 public class PairmapperStripereducer {
-	
-	 public static void main(String[] args) throws Exception {
-	        Job job = new Job(new Configuration());
-	        job.setJarByClass(PairmapperStripereducer.class);
 
-	        job.setNumReduceTasks(1);
+	public static void main(String[] args) throws Exception {
+		Job job = new Job(new Configuration());
+		job.setJarByClass(PairmapperStripereducer.class);
 
-	        job.setOutputKeyClass(Text.class);
-	        job.setOutputValueClass(Text.class);
+		job.setNumReduceTasks(1);
 
-	        job.setMapperClass(Map.class);
-	        job.setCombinerClass(Combine.class);
-	        job.setReducerClass(Reduce.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(Text.class);
 
-	        job.setInputFormatClass(TextInputFormat.class);
-	        job.setOutputFormatClass(TextOutputFormat.class);
+		job.setMapperClass(Map.class);
+		job.setCombinerClass(Combine.class);
+		job.setReducerClass(Reduce.class);
 
-	        FileInputFormat.addInputPath(job, new Path(args[0]));
-			FileOutputFormat.setOutputPath(job, new Path(args[1]));
+		job.setInputFormatClass(TextInputFormat.class);
+		job.setOutputFormatClass(TextOutputFormat.class);
 
-	        job.waitForCompletion(true);
-	    }
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
 
-	    //mapper
-	    public static class Map extends Mapper<LongWritable, Text, Text, Text> {
-	        
-	        public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-	            String[] words = value.toString().split(" ");
+		job.waitForCompletion(true);
+	}
 
-	            for (String word : words) {
-	                if (word.matches("^\\w+$")) {
-	                    int count = 0;
-	                    for (String term : words) {
-	                        if (term.matches("^\\w+$") && !term.equals(word)) {
-	                            context.write(new Text(word + "," + term), new Text("1"));
-	                            count++;
-	                        }
-	                    }
-	                    context.write(new Text(word + ",*"), new Text(String.valueOf(count)));
-	                }
-	            }
-	        }
-	    }
-	    
+	// mapper
+	public static class Map extends Mapper<LongWritable, Text, Text, Text> {
 
-	    //combiner
-	    private static class Combine extends Reducer<Text, Text, Text, Text> {
-	        
-	        public void reduce(Text key, Iterable<Text> values, Context context)
-	                throws IOException, InterruptedException {
-	            int count = 0;
-	            for (Text value : values) {
-	                count += Integer.parseInt(value.toString());
-	            }
-	            context.write(key, new Text(String.valueOf(count)));
-	        }
-	    }
-	    
+		public void map(LongWritable key, Text value, Context context)
+				throws IOException, InterruptedException {
+			String[] words = value.toString().split(" ");
 
-	
-	       
-	    //reducer
-	    public static class Reduce extends Reducer<Text, Text, Text, Text> {
-	    	
-	      
-	        public void reduce(Text key, Iterable<Text> values, Context context)
-	                throws IOException, InterruptedException {
-	        
-	            //   A10,*      {1,4} 
-	        	//   A10,A12    (1,2 }
-	        	//   A10,A14     {1,1}
-	        	 double totalCount = 0;
-	        	//initiate the HashMap stripe
-	            java.util.Map<String, Integer> stripe = new HashMap<>();
-	            
-	       
-	            String keyStr = key.toString();
-	            int count = 0;
+			for (String word : words) {
+				if (word.matches("^\\w+$")) {
+					int count = 0;
+					for (String term : words) {
+						if (term.matches("^\\w+$") && !term.equals(word)) {
+							context.write(new Text(word + "," + term),
+									new Text("1"));
+							count++;
+						}
+					}
+					context.write(new Text(word + ",*"),
+							new Text(String.valueOf(count)));
+				}
+			}
+		}
+	}
 
-	            for (Text value : values) {
-	                count += Integer.parseInt(value.toString());
-	            }
+	// combiner
+	private static class Combine extends Reducer<Text, Text, Text, Text> {
 
-	            if (!keyStr.matches(".*\\*")) {//if the key "A10,A12" finished
-	            	 String[] pair = keyStr.split(",");
-		                Integer countSum = stripe.get(pair[1]);
-	                    stripe.put(pair[1], (countSum == null ? 0 : countSum) + count);
-	                    keyStr=pair[0];
-	            } 
-	            else {
-	               
-                
-                    totalCount = count;// for a certain kind of key, 
-                    StringBuilder stripeStr = new StringBuilder();
-    	            for (java.util.Map.Entry entry : stripe.entrySet()) {
-    	            	
-    	            	double d=new Double(entry.getValue().toString())/totalCount;
-    	            	
-    	                stripeStr.append(entry.getKey()).append(":").append(String.format("%.2f", d)).append("   ");
-    	            }
-	                context.write(new Text(keyStr), new Text(stripeStr.toString()));
-	            }
+		public void reduce(Text key, Iterable<Text> values, Context context)
+				throws IOException, InterruptedException {
+			int count = 0;
+			for (Text value : values) {
+				count += Integer.parseInt(value.toString());
+			}
+			context.write(key, new Text(String.valueOf(count)));
+		}
+	}
 
-	        }
+	// reducer
+	public static class Reduce extends Reducer<Text, Text, Text, Text> {
 
-	       
+		public void reduce(Text key, Iterable<Text> values, Context context)
+				throws IOException, InterruptedException {
 
-	        class Pair implements Comparable<Pair> {
-	            double relativeFrequency;
-	            String key;
-	            String value;
+			// A10,* {1,4}
+			// A10,A12 (1,2 }
+			// A10,A14 {1,1}
+			double totalCount = 0;
+			// initiate the HashMap stripe
 
-	            Pair(double relativeFrequency, String key, String value) {
-	                this.relativeFrequency = relativeFrequency;
-	                this.key = key;
-	                this.value = value;
-	            }
+			java.util.Map<String, Integer> stripe = new HashMap<>();
 
-	            @Override
-	            public int compareTo(Pair pair) {
-	                if (this.relativeFrequency >= pair.relativeFrequency) {
-	                    return 1;
-	                } else {
-	                    return -1;
-	                }
-	            }
-	        }
-	    }
+			String keyStr = key.toString();
+			int count = 0;
+
+			for (Text value : values) {
+				count += Integer.parseInt(value.toString());
+			}
+
+			if (!keyStr.matches(".*\\*")) {// if the key "A10,A12" finished
+
+				String[] pair = keyStr.split(",");
+				keyStr = pair[0];
+				Integer countSum = stripe.get(pair[1]);
+				stripe.put(pair[1], (countSum == null ? 0 : countSum) + count);
+
+			} else {
+
+				totalCount = count;// for a certain kind of key,
+
+			}
+			
+			StringBuilder stripeStr = new StringBuilder();
+			for (java.util.Map.Entry entry : stripe.entrySet()) {
+
+				double d = new Double(entry.getValue().toString()) / totalCount;
+
+				stripeStr.append(entry.getKey()).append(":")
+						.append(String.format("%.2f", d)).append("   ");
+				context.write(new Text(keyStr), new Text(stripeStr.toString()));
+			}
+
+		}
+
+		class Pair implements Comparable<Pair> {
+			double relativeFrequency;
+			String key;
+			String value;
+
+			Pair(double relativeFrequency, String key, String value) {
+				this.relativeFrequency = relativeFrequency;
+				this.key = key;
+				this.value = value;
+			}
+
+			@Override
+			public int compareTo(Pair pair) {
+				if (this.relativeFrequency >= pair.relativeFrequency) {
+					return 1;
+				} else {
+					return -1;
+				}
+			}
+		}
+	}
 
 }
