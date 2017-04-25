696b4868048eea423759f56a8e09d1d27a9d0411
Julia Chen Jing <juliachenjing@gmail.com>
2017-04-24 21:28:09 -0500

    Update 1.java

--- 1.java
+++ 1.java
@@ -21,11 +21,23 @@
  -				String[] pair = keyStr.split(",");		
  -				newKey = pair[0];		
  -				Integer countSum = stripe.get(pair[1]);		
- -				stripe.put(pair[1], (countSum == null ? 0 : countSum) + count);		
+ -				stripe.put(pair[1], (countSum == null ? 0 : countSum) + count);	
+	 
+	                      
  -		
  -			} else {		
  -		
- -				totalCount = count;// for a certain kind of key,		
+ -				totalCount = count;// for a certain kind of key,
+	                        StringBuilder stripeStr = new StringBuilder();		
+ -			for (java.util.Map.Entry entry : stripe.entrySet()) {		
+ -		
+ -				double d = new Double(entry.getValue().toString()) / totalCount;		
+  		  
+ -				stripeStr.append(entry.getKey()).append(":")		
+ -						.append(String.format("%.2f", d)).append("   ");		
+ -			}		
+  		  
+ -			context.write(new Text(newKey), new Text(stripeStr.toString()));
  -		
  -			}		
  -		
